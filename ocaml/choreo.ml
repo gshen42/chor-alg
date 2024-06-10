cd048bf5688d54c6c201750642086992d08f7e2f
Gan Shen <gan_shen@icloud.com>
2024-06-09 21:40:19 -0700

    use a file for IPC

--- ocaml/choreo.ml
+++ ocaml/choreo.ml
@@ -17,13 +17,19 @@ let un x =
 
 (* Functions for processes to send and receive messages (in binary format) *)
 
+let buffer_file = "buffer"
+
 let send msg dst =
-  printf "* Sending a message %s to destination %s. Press ENTER to contiunue.\n" (Marshal.to_string msg []) dst
+  Out_channel.with_open_bin buffer_file (fun c ->
+    printf "* Sending a message to destination %s. Press ENTER to contiunue.\n" dst;
+    let _ = read_line () in
+    Marshal.to_channel c msg [])
 
 let recv src =
-  printf "* Receiving a message from source %s. Type in the message to continue.\n" src;
-  let s = read_line () in
-  Marshal.from_string s 0
+  In_channel.with_open_bin buffer_file (fun c -> 
+    printf "* Receiving a message from source %s. Type in the message to continue.\n" src;
+    let _ = read_line () in
+    Marshal.from_channel c)
 
 (* Choreographic operators as effects *)
 
@@ -37,13 +43,13 @@ let epp c l = match_with c () {
   effc = fun (type a) (eff : a Effect.t) ->
     match eff with
     | Comm (src, dst, msg) -> Some (fun (k : (a, _) continuation) ->
-        if src == l && dst == l
+        if src = l && dst = l
           then continue k (Present (Lazy.force msg))
-          else if src == l
+          else if src = l
             then
               let _ = send (Lazy.force msg) dst in
               continue k Absent
-            else if dst == l
+            else if dst = l
               then
                 let x = recv src in
                 continue k (Present x)
@@ -56,6 +62,6 @@ let alice, bob, carol = "alice", "bob", "carol"
 let foo () =
   let x = perform (Comm (alice, bob, lazy 42)) in
   let y = perform (Comm (bob, alice, lazy (un x + 1))) in
-  y
+  perform (Comm (alice, alice, lazy (printf "The number I receied is %n\n" (un y))))
 
-let _ = epp foo bob
+let _ = epp foo Sys.argv.(1)
